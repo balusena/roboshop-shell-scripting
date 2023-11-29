55e47326b139aa8f987ffe21b47a1e6c3a7b2a75
balusena <balusena12@gmail.com>
2023-11-29 16:27:09 +0000

    Frontend script file.

--- frontend.sh
+++ frontend.sh
@@ -1,28 +1,28 @@
 source common.sh
 
 echo -e "${color} Installing Nginx Server ${nocolor}"
-yum install nginx -y &>>/tmp/roboshop.log
+yum install nginx -y &>>$log_file
 stat_check $?
 
 echo -e "${color} Removing Old App content ${nocolor}"
-rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/* &>>$log_file
 stat_check $?
 
 echo -e "${color} Downloading Frontend Content ${nocolor}"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 stat_check $?
 
 echo -e "${color} Extract Frontend Content ${nocolor}"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>$log_file
 stat_check $?
 
 
 echo -e "${color} Update Frontend Configuration ${nocolor}"
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
 stat_check $?
 
 echo -e "${color} Starting Nginx Server ${nocolor}"
-systemctl enable nginx &>>/tmp/roboshop.log
-systemctl restart nginx &>>/tmp/roboshop.log
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
 stat_check $?
